fab099f8a7d72b327ec7673f31b0916cc59135e7
TheRohit <rohitmeshram000@gmail.com>
2023-06-04 23:47:16 +0530

    try edge function

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,3 +1,6 @@
+
+
+
 import AWS from "aws-sdk";
 import formidable from "formidable-serverless";
 import fs from "fs";
@@ -35,32 +38,27 @@ export default async function handler(req, res) {
       // read the contents of the file
       const fileContent = fs.readFileSync(video.path);
 
-      // Upload the file to S3
+      // upload the file to S3
       const putObjectParams = {
         Bucket: "evm-rohit-next-bucket",
         Key: video.name,
         Body: fileContent,
         ContentType: video.type,
       };
-      await s3.putObject(putObjectParams).promise()
-        .then(async() => {
-          console.log(`File uploaded to S3: ${video.name} (${video.type})`);
-          // send a POST request to the endpoint
-          try {
-            const response = await axios.post(localURL+"/localnode", {
-              shouldTrigger: true, // replace with your boolean value
-            });
-            console.log(response.data);
-            res.json(response.data);
-          } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: "Internal Server Error" });
-          }
-        })
-        .catch((err) => {
-          console.error(err);
-          res.status(500).json({ error: "Server error" });
+      try {
+        await s3.putObject(putObjectParams).promise();
+        console.log(`File uploaded to S3: ${video.name} (${video.type})`);
+
+        // send a POST request to the endpoint
+        const response = await axios.post(`${localURL}/localnode`, {
+          shouldTrigger: true, // replace with your boolean value
         });
+        console.log(response.data);
+        res.json({ message: "File uploaded successfully" });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
     });
   } else {
     res.status(405).json({ error: "Method not allowed" });
